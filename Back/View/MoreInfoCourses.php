09d6616aa2a5e49dceee1bc4f67bd4a4d70cbb4d
miguelonana <80260904+miguelonana@users.noreply.github.com>
2021-12-08 19:38:17 +0100

    update on backend
    update on backend

--- Back/View/MoreInfoCourses.php
+++ Back/View/MoreInfoCourses.php
@@ -1,67 +1,49 @@
 <?php
 
 include_once "../config.php";
+include_once "../Controller/GetUser.php";
 
-    function getNbUsers(){
-        $db = config::getConnexion();
-        
-        $studentQuery = "SELECT count(*) AS nb FROM student";//query to get number of students in the table
-        $teacherQuery = "SELECT count(*) AS nb FROM teacher";//query to get number of teachers in the table
+
+    function getCourses(){
+        $db = config::getconnexion();
 
         try {
-            $res = $db->query($studentQuery);
-            $data = $res->fetch();
-            $nbStudents = $data['nb'];
+            $query = $db->query(
+            "SELECT * FROM courses"
+            );
+            return $query;
 
         } catch (PDOException $e) {
             $e->getMessage();
         }
-
-        try {
-            $res = $db->query($teacherQuery);
-            $data = $res->fetch();
-            $nbTeachers = $data['nb'];
-
-        } catch (PDOException $e) {
-            $e->getMessage();
-        }
-
-        $nbUsers = $nbTeachers + $nbStudents;
-
-        return $nbUsers;
     }
 
-    function getNbOnlineUsers(){
-        $db = config::getConnexion();
-        
-        $Query = "SELECT count(*) AS nb FROM onlineUsers";//query to get number of students in the table
-        //$teacherQuery = "SELECT count(*) AS nb FROM teacher";//query to get number of teachers in the table
+    function getTeachers(){
+        $db = config::getconnexion();
 
         try {
-            $res = $db->query($Query);
-            $data = $res->fetch();
-            $nbUsers = $data['nb'];
-            
+            $query = $db->query(
+            "SELECT * FROM teacher"
+            );
+            return $query;
+
         } catch (PDOException $e) {
             $e->getMessage();
         }
-        return $nbUsers;
     }
 
-    function getNumberOfCourses(){
-        $db = config::getConnexion();
-        
-        $Query = "SELECT count(*) AS nb FROM courses";//query to get number of courses in the table
+    function getTeacherCourses($userId){
+        $db = config::getconnexion();
 
         try {
-            $res = $db->query($Query);
-            $data = $res->fetch();
-            $nbCourses = $data['nb'];
-            
+            $query = $db->query(
+            "SELECT * FROM courses where teacher='$userId'"
+            );
+            return $query;
+
         } catch (PDOException $e) {
             $e->getMessage();
         }
-        return $nbCourses;
     }
 
     $searchADM = "ADM";
@@ -70,9 +52,12 @@ include_once "../config.php";
         header('location:../../Front/View/login.html');
     }
     else{
-        $nbUsers = getNbUsers();
-        $nbOnlineUsers = getNbOnlineUsers();
-        $nbCourses = getNumberOfCourses();
+        //$CourseList = getCourses();
+        $teachersList = getTeachers();
+        if(isset($_GET['teacher']) && isset($_GET['teacher'])!="")
+            $CourseList = getTeacherCourses($_GET['teacher']);
+        else 
+            $CourseList = getCourses();
     }
 ?>
 
@@ -420,110 +405,97 @@ include_once "../config.php";
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
-
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
+                            <h1 class="m-0"></h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
+                                <li class="breadcrumb-item active">Courses DataTable</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
                 </div><!-- /.container-fluid -->
-                <div class="col-md-3 col-sm-6 col-12 float-sm-right">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">New Notifications</span>
-                            <span class="info-box-number">1,410</span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
             </div>
             <!-- /.content-header -->
 
             <!-- Main content -->
-            <section class="content" style="margin-top:5rem;">
+            <section class="content">
                 <div class="container-fluid">
                     <!-- Small boxes (Stat box) -->
                     <div class="row">
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3><?php echo $nbCourses; ?></h3>
-
-                                    <p>Number Of Courses
-                                    </p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="MoreInfoCourses.php" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
+                        <!-- Main content -->
+                        <section class="content">
+                            <div class="card-header">
+                                <center>
+                                    <!-- <h3 class="card-title">Student DataTable</h3> -->
+                                    <h3>
+                                        <img src="images/favicon.png" alt="EduEasyLogo" style="length: 7vw; width: 7vw">
+                                        Courses DataTable
+                                    </h3>
+                                </center>
                             </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3> <sup style="font-size: 20px"> <?php echo $nbOnlineUsers ?> </sup></h3>
-
-                                    <p>Online Users
-
-                                    </p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-
-                                <a href="MoreInfoOnlineUsers.php" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
+                            <div>
+                                <form action="" method="get">
+                                    <label for="">Select a Teacher:</label>
+                                    <select name="teacher" id="teacher" class="form-control" placeholder="Select a Teacher">
+                                        <option value=""></option>
+                                        <?php foreach($teachersList as $teacher){ ?>
+                                        <option value="<?php echo $teacher['userId'];?>">
+                                            <?php echo $teacher['userName'];?>
+                                        </option>
+                                        <?php } ?>
+                                    </select>
+                                    <input type="submit" value="search" class="btn btn-primary" style="margin-top:1rem;">
+                                </form>
+                                <br>
                             </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3><?php echo $nbUsers ?></h3>
+                            <div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h3 class="card-title">Courses List</h3>
+                                            </div>
+                                            <!-- /.card-header -->
+                                            <div class="card-body">
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Course Id</th>
+                                                            <th>Course Name</th>
+                                                            <th>Number of student Registered</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php foreach($CourseList as $course){ ?>
+                                                        <tr>
+                                                            <td><?php echo $course['id']?></td>
+                                                            <td><?php echo $course['name']?></td>
+                                                            <td><?php echo $course['numberOfStudentsRegistered']?></td>
+                                                        </tr>
+                                                        <?php } ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <!-- /.card-body -->
 
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="MoreInfoUserRegistration.php" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small box -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
+                                        </div>
+                                        <!-- /.card -->
 
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i
-                                        class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
 
-                        <!-- ./col -->
+                                    </div>
+                                    <!-- /.col -->
+
+                                </div>
+
+                            </div><!-- /.container-fluid -->
+                        </section>
+
+
                     </div>
                     <!-- /.row -->
                     <!-- /.row (main row) -->
